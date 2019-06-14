974df430c4775aef9b9cbda01577cdfc9fdc03b0
ben <bholsinger@gmail.com>
2019-06-14 12:52:36 -0600

    added notes to explaing first steps for server

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -2,6 +2,10 @@
 import mongoose from 'mongoose'
 
 //THIS STRING WILL CHANGE SLIGHTLY
+//this is the first file on the server side to work with.
+//here I am importing from https://cloud.mongodb.com
+//make sure password is correct and change the word test to the name of the project
+//next file to update is in Middlewear folder -session.js
 const connectionString = "mongodb://other:Student123@cluster0-shard-00-00-uoyb8.mongodb.net:27017,cluster0-shard-00-01-uoyb8.mongodb.net:27017,cluster0-shard-00-02-uoyb8.mongodb.net:27017/kanban?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 let connection = mongoose.connection
--- server/middlewear/session.js
+++ server/middlewear/session.js
@@ -1,6 +1,9 @@
 import expressSession from 'express-session'
 var mongoStore = require("connect-mongodb-session")(expressSession);
 
+//update mongo store with mongodb uri-Uniform Resource Identifier
+//copy same string from dbconfig.js
+
 var store = new mongoStore({
     uri: "mongodb://other:Student123@cluster0-shard-00-00-uoyb8.mongodb.net:27017,cluster0-shard-00-01-uoyb8.mongodb.net:27017,cluster0-shard-00-02-uoyb8.mongodb.net:27017/kanban?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", //CHANGE ME!!!!!!
     collection: "Sessions"
