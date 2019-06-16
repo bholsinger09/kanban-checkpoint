6a71e09ccfeea61d02063d386a229b497eca5701
ben <bholsinger@gmail.com>
2019-06-16 15:10:38 -0600

    removed list description

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="board container-fluid">
     <!-- <header class="row"> -->
-      <form class="new-list row" @submit.prevent="createList">
-        <input type="text" class="form-control col-4 offset-1" placeholder="list title" v-model="newList.title" Required>
-        <!-- <input type="text" class="form-control col-4 offset-1" placeholder="description" v-model="newList.description" Required> -->
-        <button  type="submit" class="col-1 btn btn-secondary btn-md rounded">Create List</button>
-      </form>
+    <form class="new-list row" @submit.prevent="createList">
+      <input type="text" class="form-control col-4 offset-1" placeholder="list title" v-model="newList.title" Required>
+      <button type="submit" class="col-1 btn btn-secondary btn-md rounded">Create List</button>
+    </form>
     <!-- </header> -->
     <div>
       <h2>{{board.title}}</h2>
@@ -28,7 +27,6 @@
       return {
         newList: {
           title: '',
-          description: '',
           boardId: this.boardId
         }
       }
@@ -47,14 +45,14 @@
         }
       },
       lists() {
-        return this.$store.state.lists;  //this look like above?
+        return this.$store.state.lists;
       },
     },
     methods: {
       createList() {
         this.$store.dispatch('createList', this.newList)
-      //   this.newList.title = ''
-      //   this.newList.description = ''
+        //   this.newList.title = ''
+
       }
     }
   };
@@ -73,23 +71,28 @@
   .list-look h1 {
     color: rgb(255, 255, 255);
   }
+
   .form-control {
     background-color: #700000b7;
-  color: #0000ff;
+    color: #0000ff;
   }
+
   .form-control:active {
-background-color: #700000b7;
+    background-color: #700000b7;
   }
+
   button {
     color: #ae00ff;
     background-color: #0000ff25;
     font-weight: bold;
     /* border: #ae00ffd7 5px solid; */
-  } 
+  }
+
   button:active {
     background-color: #0000ff;
   }
+
   button:hover {
     background-color: #ae00ffd7;
-  } 
+  }
 </style>
